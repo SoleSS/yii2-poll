0ee401a168e11021a20d483a3be39e1b90f0b000
Michael Nordheimer <nordheimerm@gmail.com>
2020-02-29 09:08:46 +0300

    widget fix

--- widgets/views/amp-poll.php
+++ widgets/views/amp-poll.php
@@ -13,35 +13,44 @@ use yii\helpers\Url;
     "showOptions": 1
 </amp-state>
 
-<div class="options-wrap">
-
-    <?php $formModel = new \soless\poll\models\VoteForm(); $formModel->pollId = $model->id; ?>
-    <?php $form = ActiveForm::begin([
-        'id' => 'poll-'. $model->id,
-        'enableAjaxValidation' => false,
-        'enableClientValidation' => false,
-        'enableClientScript' => false,
-        'method' => 'post',
-        'options' => [
-            'action-xhr' => \Yii::$app->UrlManager->hostInfo . Url::toRoute(['/ps/api/vote']),
-            'target' => '_blank',
-        ],
-    ]); ?>
-
-    <?= $form->field($formModel, 'pollId')->hiddenInput()->label(false) ?>
-
-    <div class="radios-wrap" [class]="poll_<?= $model->id ?>.showOptions ? 'radios-wrap' : 'radios-wrap hide'">
-        <?= $form->field($formModel, 'pollItemId')->radioList($model->optionsArray, [
-            'itemOptions' => [
-                'on' => 'change:'. 'poll-'. $model->id .'.submit,AMP.setState({poll_'. $model->id .': { showOptions: 0 } })'
-            ],
-        ])->label(false) ?>
+<div class="clearfix">
+    <div class="title-wrap">
+        <h3><?= $model->title ?></h3>
+    </div>
+    <div class="description-wrap">
+        <?= $model->description ?>
     </div>
 
+    <?php if (!(!\Yii::$app->user->isGuest && in_array(\Yii::$app->user->id, $model->todayVotedUsers)) && !\Yii::$app->session->has('poll-'.$model->id)) : ?>
+        <div class="options-wrap">
 
-    <div submit-success>
-        <script type="text/plain"
-                template="amp-mustache">
+            <?php $formModel = new \soless\poll\models\VoteForm(); $formModel->pollId = $model->id; ?>
+            <?php $form = ActiveForm::begin([
+                'id' => 'poll-'. $model->id,
+                'enableAjaxValidation' => false,
+                'enableClientValidation' => false,
+                'enableClientScript' => false,
+                'method' => 'post',
+                'options' => [
+                    'action-xhr' => \Yii::$app->UrlManager->hostInfo . Url::toRoute(['/ps/api/vote']),
+                    'target' => '_blank',
+                ],
+            ]); ?>
+
+            <?= $form->field($formModel, 'pollId')->hiddenInput()->label(false) ?>
+
+            <div class="radios-wrap" [class]="poll_<?= $model->id ?>.showOptions ? 'radios-wrap' : 'radios-wrap hide'">
+                <?= $form->field($formModel, 'pollItemId')->radioList($model->optionsArray, [
+                    'itemOptions' => [
+                        'on' => 'change:'. 'poll-'. $model->id .'.submit,AMP.setState({poll_'. $model->id .': { showOptions: 0 } })'
+                    ],
+                ])->label(false) ?>
+            </div>
+
+
+            <div submit-success>
+                <script type="text/plain"
+                        template="amp-mustache">
               <p>{{caption}}</p>
               <p>Результаты</p>
               <p>Всего голосов: {{#totalVotes}}</p>
@@ -58,11 +67,18 @@ use yii\helpers\Url;
                 {{/data}}
               </table>
             </script>
-    </div>
-    <div submit-error>
-        Что-то пошло не так и ваш голос не удалось сохранить... {{error}}
-    </div>
+            </div>
+            <div submit-error>
+                Что-то пошло не так и ваш голос не удалось сохранить... {{error}}
+            </div>
 
-    <?php ActiveForm::end(); ?>
+            <?php ActiveForm::end(); ?>
 
+        </div>
+    <?php else : ?>
+        <div class="results-wrap">
+            Results
+        </div>
+    <?php endif; ?>
 </div>
+
