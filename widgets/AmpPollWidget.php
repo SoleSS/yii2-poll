c846ce523c279cad7e2468e6d72a9b4c8b8251a7
Michael Nordheimer <nordheimerm@gmail.com>
2020-02-29 20:10:21 +0300

    + layout param

--- widgets/AmpPollWidget.php
+++ widgets/AmpPollWidget.php
@@ -6,15 +6,19 @@ use soless\poll\models\PsPoll;
 class AmpPollWidget extends \yii\base\Widget
 {
     public $model;
+    public $layout;
 
     public function init()
     {
         parent::init();
+        if ($this->layout === null) {
+            $this->layout = 'amp-poll';
+        }
     }
 
     public function run()
     {
-        return $this->render('amp-poll', [
+        return $this->render($this->layout, [
             'model' => $this->model,
         ]);
     }
